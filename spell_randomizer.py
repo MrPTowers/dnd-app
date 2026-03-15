85ad0770101579abfdfdd90af599e80fc2d033ad
MrPTowers <pablourayoan@gmail.com>
2026-03-15 18:09:04 -0400

    First version. Successfull spell randomization with input of spell amount and list

---
+++ spell_randomizer.py
@@ -0,0 +1,32 @@
+import random
+import sys
+
+spell_amount = int(sys.argv[1])
+file = sys.argv[2]
+
+
+def main():
+    global spell_amount
+    spells = []
+
+    with open(file, 'r') as f:
+        for line in f:
+            spells.append(line.strip())
+
+    randomized_spells = []
+
+    while spell_amount > 0:
+
+        random_spell = random.choice(spells)
+        randomized_spells.append(random_spell)
+
+        spells.remove(random_spell)
+        spell_amount = spell_amount - 1
+
+    print(randomized_spells)
+
+if __name__ == "__main__":
+    if len(sys.argv) == 3:
+        main()
+    else:
+        print("Usage: python3 spell_randomizer.py <spell_amount> <spells_file>")
