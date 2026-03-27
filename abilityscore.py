750422bbb6d4d95f97178ad9b6f2f952d6130654
MrPTowers <pablourayoan@gmail.com>
2026-03-27 19:43:47 -0400

    adding dc and ability score generators

---
+++ abilityscore.py
@@ -0,0 +1,15 @@
+import random
+
+def roll_scores():
+    scores = [random.randint(3, 18) for _ in range(6)]
+    print(*scores)
+    return sum(scores)
+
+print("DND Ability Score Roller\n")
+
+while True:
+    total = roll_scores()
+    while not (72 <= total <= 90):
+        total = roll_scores()
+    if input("Continue? (y/n) ").lower() == "n":
+        break
